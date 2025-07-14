eff67e64a43bffaa695c50d1027c14a144ffab9a
Zumbuh <mcbronydiamond@gmail.com>
2025-07-14 23:18:14 +0200

    Target 1.21.7 & remove LitematicTest

--- src/main/java/net/jagsnet/minecraft/plugins/dungeons/Dungeons.java
+++ src/main/java/net/jagsnet/minecraft/plugins/dungeons/Dungeons.java
@@ -2,7 +2,6 @@ package net.jagsnet.minecraft.plugins.dungeons;
 
 import net.jagsnet.minecraft.plugins.dungeons.commands.Main;
 import net.jagsnet.minecraft.plugins.dungeons.listeners.Events;
-import net.jagsnet.minecraft.plugins.dungeons.listeners.LitematicTest;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.scheduler.BukkitScheduler;
 
@@ -18,7 +17,6 @@ public final class Dungeons extends JavaPlugin {
         // Plugin startup logic
         instance = this;
         this.getCommand("dungeons").setExecutor(new Main());
-        getServer().getPluginManager().registerEvents(new LitematicTest(), this);
 
         BukkitScheduler scheduler = getServer().getScheduler();
         scheduler.scheduleSyncRepeatingTask(Dungeons.getInstance(), new Runnable() {
